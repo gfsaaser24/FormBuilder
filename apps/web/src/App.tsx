8317b51a8d57e67d307ef3fe2071d70aa2d4916b
Gabe Fletcher <gabe@editmypodcast.agency>
2025-05-04 19:28:06 -0400

    Set up Vite project structure and fix dependencies

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,23 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import { SiteHeader } from './components/site-header';
-import { TailwindIndicator } from './components/tailwind-indicator';
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/shared/Toaster";
+import { SiteHeader } from '@/components/site-header';
 import HomePage from './pages/index/HomePage';
 import BuilderPage from './pages/builder/BuilderPage';
 import TemplatesPage from './pages/templates/TemplatesPage';
+import { TailwindIndicator } from '@/components/tailwind-indicator';
+import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 
 function App() {
   return (
-    <>
-      <SiteHeader />
-      <div className="flex-1">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/builder" element={<BuilderPage />} />
-          <Route path="/templates" element={<TemplatesPage />} />
-        </Routes>
-      </div>
-      <TailwindIndicator />
-    </>
+    <div className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <div className="relative flex min-h-screen flex-col">
+          <SiteHeader />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/builder" element={<BuilderPage />} />
+              <Route path="/templates" element={<TemplatesPage />} />
+            </Routes>
+          </div>
+          <Toaster />
+        </div>
+        <TailwindIndicator />
+      </ThemeProvider>
+    </div>
   );
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
-import { ThemeProvider } from './components/theme-provider';
-import { Toaster } from './components/shared/Toaster';
-import { fontSans } from './lib/fonts';
-import { cn } from './lib/utils';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <div className="relative flex min-h-screen flex-col">
-            <App />
-            <Toaster />
-          </div>
-        </ThemeProvider>
-      </div>
-    </React.StrictMode>
-  </BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
